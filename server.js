f2ccdb59f8ea08e4c562a2e3004758ced2fd73a0
Hyun <hwn123h@naver.com>
2024-06-13 20:30:25 +0900

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
  */
 
 app.use(cors({
-    origin: 'https://figureinfo.netlify.app'
+    origin: 'https://figureinfo.netlify.app/'
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
